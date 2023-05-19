bb6d32ffb067bfbfcc8d269c1e405b4c633eb502
Madhab Tripathy <66151264+madhab-tripathy@users.noreply.github.com>
2023-05-19 23:37:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+function select_remove(){
+    let colorSelector = document.getElementById("colorSelect")
+    colorSelector.options[colorSelector.selectedIndex].remove();   
+}
